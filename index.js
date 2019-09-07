69ee1d2c2c8fe31d938883cea0d5d6c3a36bc207
G Sriram <sgcuber24@gmail.com>
2019-09-07 19:05:11 +0530

    .

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const morgan = require('morgan');
 app.use(morgan('dev'));
 app.use(express.json());
 
-const busRouter = require('./routes/busRoute')
+const busRouter = require('./routes/busRoute');
 app.use('/api/v1/busses', busRouter);
-module.exports = app
+module.exports = app;
