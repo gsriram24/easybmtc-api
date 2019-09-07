2f60e924ac4442de8ea0f9eb67011b07604245b6
G Sriram <sgcuber24@gmail.com>
2019-09-07 19:27:47 +0530

    Root file routing and indentation fixes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
@@ -6,5 +7,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 const busRouter = require('./routes/busRoute');
+app.get('/', function(req, res) {
+  res.send('Welcome to this API! Navigate to /api/v1/busses/:busReg to view the results!');
+});
 app.use('/api/v1/busses', busRouter);
 module.exports = app;
--- server.js
+++ server.js
@@ -1,22 +1,23 @@
+/* eslint-disable max-len */
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({
-	path: './config.env'
+  path: './config.env',
 });
 const app = require('./index');
 const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
 
 mongoose
-	.connect(DB, {
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useFindAndModify: false
-	})
-	.then((con) => {
-		console.log('DB Connected');
-	});
+    .connect(DB, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then((con) => {
+      console.log('DB Connected');
+    });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...`);
 });
