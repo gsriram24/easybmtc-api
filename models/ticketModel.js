cea55bbf976207782d8661dfc7ab1f38518c8a80
G Sriram <sgcuber24@gmail.com>
2019-11-19 09:18:23 +0530

    Bug fix in ticket model

--- models/ticketModel.js
+++ models/ticketModel.js
@@ -1,63 +1,62 @@
 const mongoose = require('mongoose');
 
-const ticketSchema = new mongoose.Schema({
-  busNo: {
-    type: String,
-    required: [true, 'A Bus must have a Number'],
-    trim: true,
-  },
-  busReg: {
-    type: String,
-    required: [true, 'A Bus must have a Registration Number'],
-    unique: true,
-    trim: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now(),
-    select: true,
-  },
-  from: {
-    type: String,
-    required: [true, 'A ticket must have a From'],
-  },
-  to: {
-    type: String,
-    required: [true, 'A ticket must have a To'],
-  },
-  numberAdults: {
-    type: Number,
-    default: 0,
-  },
-  numberChildren: {
-    type: Number,
-    default: 0,
-  },
-  numberSC: {
-    type: Number,
-    default: 0,
-  },
-  fare: {
-    type: Number,
-    required: [true, 'A ticket must have a price'],
-  },
-  txnId: {
-    type: String,
-  },
-
-},
-{
-  toJSON: {
-    virtuals: true,
-  },
-  toObject: {
-    virtuals: true,
-  },
-}
+const ticketSchema = new mongoose.Schema(
+	{
+		busNo: {
+			type: String,
+			required: [ true, 'A Bus must have a Number' ],
+			trim: true
+		},
+		busReg: {
+			type: String,
+			required: [ true, 'A Bus must have a Registration Number' ],
+			trim: true
+		},
+		date: {
+			type: Date,
+			default: Date.now(),
+			select: true
+		},
+		from: {
+			type: String,
+			required: [ true, 'A ticket must have a From' ]
+		},
+		to: {
+			type: String,
+			required: [ true, 'A ticket must have a To' ]
+		},
+		numberAdults: {
+			type: Number,
+			default: 0
+		},
+		numberChildren: {
+			type: Number,
+			default: 0
+		},
+		numberSC: {
+			type: Number,
+			default: 0
+		},
+		fare: {
+			type: Number,
+			required: [ true, 'A ticket must have a price' ]
+		},
+		txnId: {
+			type: String
+		}
+	},
+	{
+		toJSON: {
+			virtuals: true
+		},
+		toObject: {
+			virtuals: true
+		}
+	}
 );
 
 ticketSchema.pre('save', function() {
-  console.log(this);
+	console.log(this);
 });
 const ticket = mongoose.model('ticket', ticketSchema);
 
